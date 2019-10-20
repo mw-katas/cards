63cb41f2c5f6db566fde92ad8dc663e629771c31
Michał Woźniczak <michal.wozniczak@gmail.com>
2019-10-20 22:49:32 +0200

    Added docs+doctest, and a couple helpful methods

--- cards/common.py
+++ cards/common.py
@@ -1,6 +1,38 @@
+def get_faces():
+    """
+    Returns all standard playing card faces, starting at 2:
+
+    >>> get_faces()
+    [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
+    """
+    return list(range(2,11)) + ['J', 'Q', 'K', 'A']
+
 def get_deck():
+    """
+    Returns a standard deck of 52 playing cards:
+
+    >>> get_deck()
+    ['2♠', '2♥', '2♦', '2♣', '3♠', '3♥', '3♦', '3♣', '4♠', '4♥', '4♦', '4♣', '5♠', '5♥', '5♦', '5♣', '6♠', '6♥', '6♦', '6♣', '7♠', '7♥', '7♦', '7♣', '8♠', '8♥', '8♦', '8♣', '9♠', '9♥', '9♦', '9♣', '10♠', '10♥', '10♦', '10♣', 'J♠', 'J♥', 'J♦', 'J♣', 'Q♠', 'Q♥', 'Q♦', 'Q♣', 'K♠', 'K♥', 'K♦', 'K♣', 'A♠', 'A♥', 'A♦', 'A♣']
+
+    >>> len(get_deck())
+    52
+    """
     return [
         f'{number}{suit}'
-        for number in list(range(2,11)) + ['J', 'Q', 'K', 'A']
-        for suit in ['♠', '♥', '♦', '♣']
+        for number in get_faces()
+        for suit in ('♠', '♥', '♦', '♣')
     ]
+
+def get_suit(card: str):
+    """
+    Returns a suit of a given card:
+
+    >>> get_suit('A♣')
+    '♣'
+    >>> get_suit('10♣')
+    '♣'
+    """
+    return card[-1]
+
+def get_face(card: str):
+    return card[:-1]
